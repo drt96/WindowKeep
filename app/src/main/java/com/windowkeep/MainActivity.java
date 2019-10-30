fb2fa9072b6089e6122c2f7d309f56737b6389e6
akoch <akoch2501@live.com>
2019-10-30 11:00:52 -0600

    Added Toast for testing

--- app/src/main/java/com/windowkeep/MainActivity.java
+++ app/src/main/java/com/windowkeep/MainActivity.java
@@ -3,6 +3,7 @@ package com.windowkeep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "Clone of project successful", Toast.LENGTH_LONG).show();
     }
 }
