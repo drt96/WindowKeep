40782d7cd9cb3c990c28b85af99c75d15fbbf895
drt96 <38338242+drt96@users.noreply.github.com>
2019-11-06 10:51:54 -0700

    Cleared souts

--- app/src/main/java/com/windowkeep/MainActivity.java
+++ app/src/main/java/com/windowkeep/MainActivity.java
@@ -20,11 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hello everyone from Daniel");
-        System.out.println("Hello");
-        System.out.println("Tanner says hello");
-        System.out.println("Aaron says hello");
-
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
