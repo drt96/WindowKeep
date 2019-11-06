f40fc436e53f366c90a52726187ff1a9b169ce54
drt96 <38338242+drt96@users.noreply.github.com>
2019-11-06 11:06:42 -0700

    Added private data to Date.class

--- app/src/main/java/com/windowkeep/Date.java
+++ app/src/main/java/com/windowkeep/Date.java
@@ -1,4 +1,10 @@
 package com.windowkeep;
 
 public class Date {
+    private int day;
+    private int month;
+    private int year;
+    // Use built in machine time TODO figure out how to do this
+    private int time;
+
 }
