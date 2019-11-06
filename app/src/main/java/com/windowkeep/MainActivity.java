87d457597a832b12d2ec44836ebf69badbcd6c0c
akoch <akoch2501@live.com>
2019-11-06 08:50:55 -0700

    Finished adding FireBase to project. Tested with simple message to cloud database, tested OK!

--- app/src/main/java/com/windowkeep/MainActivity.java
+++ app/src/main/java/com/windowkeep/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Hello");
         System.out.println("Tanner says hello");
         System.out.println("Aaron says hello");
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
