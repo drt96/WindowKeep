52111feae14869c86f8876f918fcb9c385559a70
drt96 <38338242+drt96@users.noreply.github.com>
2019-11-06 11:17:13 -0700

    added data to Customer.class

--- app/src/main/java/com/windowkeep/Customer.java
+++ app/src/main/java/com/windowkeep/Customer.java
@@ -1,4 +1,8 @@
 package com.windowkeep;
 
 public class Customer {
+    private Date qouteDate;
+    private Customer customer;
+    private Dollar dollar;
+    private WindowDetails windowDetails;
 }
