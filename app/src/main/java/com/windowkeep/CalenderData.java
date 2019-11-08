4fa55ab7aa4391e44f3f66bc896f9f4abfa55d34
drt96 <38338242+drt96@users.noreply.github.com>
2019-11-08 10:55:33 -0700

    Update minimum requirements while working on the date class

--- app/src/main/java/com/windowkeep/CalenderData.java
+++ app/src/main/java/com/windowkeep/CalenderData.java
@@ -7,5 +7,5 @@ import java.util.List;
  A class to hold a list of Calender appointments
  */
 public class CalenderData {
-   private List appointments;
+   private List <Appointment> appointments;
 }
--- app/src/main/java/com/windowkeep/Date.java
+++ app/src/main/java/com/windowkeep/Date.java
@@ -1,13 +1,51 @@
 package com.windowkeep;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
+// Time zone America/Boise
 /*
     Handles the logic to make and use Dates and Times
  */
 public class Date {
-    private int day;
-    private int month;
-    private int year;
-    // Use built in machine time TODO figure out how to do this
-    private int time;
+    private String day;
+    private String month;
+    private String year;
+    private String time;
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime( FormatStyle.MEDIUM );
+    String output = zdt.format( formatter.withLocale( Locale.US ) );  // Or Locale.CANADA_FRENCH and so on.
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
