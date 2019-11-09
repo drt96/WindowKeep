f0d7bf99e7aaffc254cc0547977ec8ada8959f4e
tannerbeeson <bee15005@byui.edu>
2019-11-08 17:33:57 -0700

    Added a blank SelectDate_View. This will hold a calendar view to select a date for an appointment. The selected date will be sent to the CreateQuote_View

---
+++ app/src/main/java/com/windowkeep/SelectDate_View.java
@@ -0,0 +1,16 @@
+package com.windowkeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectDate_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_date__view);
+
+
+    }
+}
