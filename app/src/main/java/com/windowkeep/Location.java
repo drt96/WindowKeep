83ba351cbf81f4345ea16ccd87f7fd95295151e1
drt96 <38338242+drt96@users.noreply.github.com>
2019-11-08 17:39:01 -0700

    Working on the Location class. Think I've got it all

--- app/src/main/java/com/windowkeep/Location.java
+++ app/src/main/java/com/windowkeep/Location.java
@@ -7,5 +7,38 @@ public class Location {
     private double Latitude;
     private double Longitude;
 
+    /* Constructors */
+    public Location() {
+        Latitude = 0;
+        Longitude = 0;
+    }
 
+    public Location(double latitude, double longitude) {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    /* Getters and Setters */
+    public double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        Latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        Longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Location: " +
+                "Latitude: " + Latitude +
+                ", Longitude: " + Longitude;
+    }
 }
