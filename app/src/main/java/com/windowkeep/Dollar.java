2a037c6cef5d00c1142e70c368ddd21011cb141d
drt96 <38338242+drt96@users.noreply.github.com>
2019-11-09 21:18:12 -0700

    Working on the Dollar class. Think I've got it all

--- app/src/main/java/com/windowkeep/Dollar.java
+++ app/src/main/java/com/windowkeep/Dollar.java
@@ -1,9 +1,39 @@
 package com.windowkeep;
 
 /*
-    Handles the logic to make and use dollars
+    Handles the logic to make and use US dollars
+
+    For now the class is pretty arbitrary, but can quickly be modified
+    to handle addition and subtraction of $'s
  */
 public class Dollar {
-    private int wholeNumber;
+    private long wholeNumber;
     private int cents;
+
+    public Dollar(long wholeNumber, int cents) {
+        this.wholeNumber = wholeNumber;
+        this.cents = cents;
+    }
+
+    @Override
+    public String toString() {
+        return "$ " + wholeNumber +
+                "." + cents;
+    }
+
+    public long getWholeNumber() {
+        return wholeNumber;
+    }
+
+    public void setWholeNumber(long wholeNumber) {
+        this.wholeNumber = wholeNumber;
+    }
+
+    public int getCents() {
+        return cents;
+    }
+
+    public void setCents(int cents) {
+        this.cents = cents;
+    }
 }
