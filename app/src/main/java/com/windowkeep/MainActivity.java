9b08e0d687eff8e25059cdefcd382769a459cda1
dkarnafel <kar15002@byui.edu>
2019-11-13 22:05:54 -0700

    Added a map object in the xml for main activity. Added in MainActivity the basic setup for a map

--- app/src/main/java/com/windowkeep/MainActivity.java
+++ app/src/main/java/com/windowkeep/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,10 +18,17 @@ public class MainActivity extends AppCompatActivity {
     // Create variable for button
     private Button quoteButton;
 
+    // Constraint Variable for MapView
+    private MapView mMapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set up for Map
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -37,5 +47,41 @@ public class MainActivity extends AppCompatActivity {
     private void createQuote() {
         Intent intent = new Intent(this, CreateQuote_View.class);
         startActivity(intent);
+    }
+
+    // Main function that sets the initial long and lat for the map as well as its view type
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 43.8231;
+            double longitude = -111.7924;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+    // Override functions for the Arcgis map
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
     }
 }
