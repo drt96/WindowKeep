9c996da29efa08479d03c015fdb1f38c98bc273a
dkarnafel <kar15002@byui.edu>
2019-11-13 22:15:24 -0700

    Map now shows the current location of the user.

--- app/src/main/java/com/windowkeep/MainActivity.java
+++ app/src/main/java/com/windowkeep/MainActivity.java
@@ -1,14 +1,21 @@
 package com.windowkeep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Constraint Variable for MapView
     private MapView mMapView;
+    private LocationDisplay mLocationDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         //Set up for Map
         mMapView = findViewById(R.id.mapView);
         setupMap();
+        setupLocationDisplay();
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -83,5 +92,39 @@ public class MainActivity extends AppCompatActivity {
             mMapView.dispose();
         }
         super.onDestroy();
+    }
+
+    // This function is what asks for the user permission to access location and then show where they are located at.
+    // Not sure if we want to prompt for permission or just do it automatically. For now it asks for permission.
+    private void setupLocationDisplay() {
+        mLocationDisplay = mMapView.getLocationDisplay();
+        mLocationDisplay.addDataSourceStatusChangedListener(dataSourceStatusChangedEvent -> {
+            if (dataSourceStatusChangedEvent.isStarted() || dataSourceStatusChangedEvent.getError() == null) {
+                return;
+            }
+
+            int requestPermissionsCode = 2;
+            String[] requestPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+            if (!(ContextCompat.checkSelfPermission(MainActivity.this, requestPermissions[0]) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(MainActivity.this, requestPermissions[1]) == PackageManager.PERMISSION_GRANTED)) {
+                ActivityCompat.requestPermissions(MainActivity.this, requestPermissions, requestPermissionsCode);
+            } else {
+                String message = String.format("Error in DataSourceStatusChangedListener: %s",
+                        dataSourceStatusChangedEvent.getSource().getLocationDataSource().getError().getMessage());
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+        mLocationDisplay.startAsync();
+    }
+    // Override for if the user chooses to not give location
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mLocationDisplay.startAsync();
+        } else {
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.location_permission_denied), Toast.LENGTH_SHORT).show();
+        }
     }
 }
