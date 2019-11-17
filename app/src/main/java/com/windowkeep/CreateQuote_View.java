613a04f82da9ccab96cce20b47e1ffe2395ccd9a
tannerbeeson <bee15005@byui.edu>
2019-11-16 19:41:52 -0700

    Functionality of changing the number of windows text when changing from basement to 1 etc is currently broken, but the logic is close

--- app/src/main/java/com/windowkeep/CreateQuote_View.java
+++ app/src/main/java/com/windowkeep/CreateQuote_View.java
@@ -7,14 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -43,10 +42,6 @@ public class CreateQuote_View extends AppCompatActivity implements AdapterView.O
     private static int comM;
     private static int comL;
 
-
-    public CreateQuote_View(String custFirstName, String toString) {
-    }
-
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,14 +58,12 @@ public class CreateQuote_View extends AppCompatActivity implements AdapterView.O
 
         database = FirebaseDatabase.getInstance();
 
-
         // Setting up the spinner for selecting which floor to input a number of windows on
         Spinner floorsSpinner = findViewById(R.id.s_floors);
         ArrayAdapter<CharSequence> floorAdapter = ArrayAdapter.createFromResource(this, R.array.numFloors, android.R.layout.simple_spinner_item);
         floorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         floorsSpinner.setAdapter(floorAdapter);
         floorsSpinner.setOnItemSelectedListener(this);
-
 
         // A listener for when number of windows editText changes
         TextWatcher textWatcher = new TextWatcher() {
@@ -86,25 +79,101 @@ public class CreateQuote_View extends AppCompatActivity implements AdapterView.O
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (floorsSpinner.getSelectedItem() == "Basement") {
-                    bS = Integer.parseInt(small_windows.getText().toString());
-                    bM = Integer.parseInt(medium_windows.getText().toString());
-                    bL = Integer.parseInt(large_windows.getText().toString());
+                String currentFloor = floorsSpinner.getSelectedItem().toString();
+
+                if (currentFloor.equalsIgnoreCase("Basement")) {
+                    if (!small_windows.getText().toString().equalsIgnoreCase("")) {
+                        bS = Integer.parseInt(small_windows.getText().toString());
+                    }
+                    else {
+                        //bS = 0;
+                    }
+
+                    if (!medium_windows.getText().toString().equalsIgnoreCase("")) {
+                        bM = Integer.parseInt(medium_windows.getText().toString());
+                    }
+                    else {
+                        //bM = 0;
+                    }
+
+                    if (!large_windows.getText().toString().equalsIgnoreCase("")) {
+                        bL = Integer.parseInt(large_windows.getText().toString());
+                    }
+                    else {
+                        //bL = 0;
+                    }
+                    Log.i("Pants", currentFloor + " window variables updated");
                 }
-                else if (floorsSpinner.getSelectedItem() == "1") {
-                    oneS = Integer.parseInt(small_windows.getText().toString());
-                    oneM = Integer.parseInt(medium_windows.getText().toString());
-                    oneL = Integer.parseInt(large_windows.getText().toString());
+                else if (currentFloor.equalsIgnoreCase("1")) {
+                    if (!small_windows.getText().toString().equalsIgnoreCase("")) {
+                        oneS = Integer.parseInt(small_windows.getText().toString());
+                    }
+                    else {
+                        //oneS = 0;
+                    }
+
+                    if (!medium_windows.getText().toString().equalsIgnoreCase("")) {
+                        oneM = Integer.parseInt(medium_windows.getText().toString());
+                    }
+                    else {
+                        //oneM = 0;
+                    }
+
+                    if (!large_windows.getText().toString().equalsIgnoreCase("")) {
+                        oneL = Integer.parseInt(large_windows.getText().toString());
+                    }
+                    else {
+                        //oneL = 0;
+                    }
+                    Log.i("Pants", currentFloor + " window variables updated");
                 }
-                else if (floorsSpinner.getSelectedItem() == "2") {
-                    twoS = Integer.parseInt(small_windows.getText().toString());
-                    twoM = Integer.parseInt(medium_windows.getText().toString());
-                    twoL = Integer.parseInt(large_windows.getText().toString());
+                else if (currentFloor.equalsIgnoreCase("2")) {
+                    if (!small_windows.getText().toString().equalsIgnoreCase("")) {
+                        twoS = Integer.parseInt(small_windows.getText().toString());
+                    }
+                    else {
+                        //twoS = 0;
+                    }
+
+                    if (!medium_windows.getText().toString().equalsIgnoreCase("")) {
+                        twoM = Integer.parseInt(medium_windows.getText().toString());
+                    }
+                    else {
+                        //twoM = 0;
+                    }
+
+                    if (!large_windows.getText().toString().equalsIgnoreCase("")) {
+                        twoL = Integer.parseInt(large_windows.getText().toString());
+                    }
+                    else {
+                        //twoL = 0;
+                    }
+
+                    Log.i("Pants", currentFloor + " window variables updated");
                 }
-                else if (floorsSpinner.getSelectedItem() == "Commercial") {
-                    comS = Integer.parseInt(small_windows.getText().toString());
-                    comM = Integer.parseInt(medium_windows.getText().toString());
-                    comL = Integer.parseInt(large_windows.getText().toString());
+                else if (currentFloor.equalsIgnoreCase("Commercial")) {
+                    if (!small_windows.getText().toString().equalsIgnoreCase("")) {
+                        comS = Integer.parseInt(small_windows.getText().toString());
+                    }
+                    else {
+                        //comS = 0;
+                    }
+
+                    if (!medium_windows.getText().toString().equalsIgnoreCase("")) {
+                        comM = Integer.parseInt(medium_windows.getText().toString());
+                    }
+                    else {
+                        //comM = 0;
+                    }
+
+                    if (!large_windows.getText().toString().equalsIgnoreCase("")) {
+                        comL = Integer.parseInt(large_windows.getText().toString());
+                    }
+                    else {
+                        //comL = 0;
+                    }
+
+                    Log.i("Pants", currentFloor + " window variables updated");
                 }
             }
         };
@@ -116,27 +185,41 @@ public class CreateQuote_View extends AppCompatActivity implements AdapterView.O
     }
 
 
+    public CreateQuote_View() {
+    }
+
+    public CreateQuote_View(String custFirstName, String toString) {
+    }
+
+
     @Override // This funciton is an abstract function from the spinner that needs implementation
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if (parent.getSelectedItem() == "Basement") {
-            small_windows.setText(bS);
-            medium_windows.setText(bM);
-            large_windows.setText(bL);
+        Spinner floorsSpinner = findViewById(R.id.s_floors);
+
+        String currentFloor = floorsSpinner.getSelectedItem().toString();
+
+        showVariables();
+
+        if (floorsSpinner.getSelectedItem().toString().equalsIgnoreCase("Basement")) {
+            small_windows.setText("" + bS);
+            medium_windows.setText("" + bM);
+            large_windows.setText("" + bL);
+            Log.i("Pants", "Basement window Text updated");
         }
-        else if (parent.getSelectedItem() == "1") {
-            small_windows.setText(oneS);
-            medium_windows.setText(oneM);
-            large_windows.setText(oneL);
+        else if (floorsSpinner.getSelectedItem().toString().equalsIgnoreCase("1")) {
+            small_windows.setText("" + oneS);
+            medium_windows.setText("" + oneM);
+            large_windows.setText("" + oneL);
         }
-        else if (parent.getSelectedItem() == "2") {
-            small_windows.setText(twoS);
-            medium_windows.setText(twoM);
-            large_windows.setText(twoL);
+        else if (floorsSpinner.getSelectedItem().toString().equalsIgnoreCase("2")) {
+            small_windows.setText("" + twoS);
+            medium_windows.setText("" + twoM);
+            large_windows.setText("" + twoL);
         }
-        else if (parent.getSelectedItem() == "Commercial") {
-            small_windows.setText(comS);
-            medium_windows.setText(comM);
-            large_windows.setText(comL);
+        else if (floorsSpinner.getSelectedItem().toString().equalsIgnoreCase("Commercial")) {
+            small_windows.setText("" + comS);
+            medium_windows.setText("" + comM);
+            large_windows.setText("" + comL);
         }
     }
 
@@ -160,6 +243,25 @@ public class CreateQuote_View extends AppCompatActivity implements AdapterView.O
         comS = 0;
         comM = 0;
         comL = 0;
+    }
+
+
+    public void showVariables() {
+        Log.i("Pants", "bS: " + bS);
+        Log.i("Pants", "bM: " + bM);
+        Log.i("Pants", "bL: " + bL);
+
+        Log.i("Pants", "oneS: " + oneS);
+        Log.i("Pants", "oneM: " + oneM);
+        Log.i("Pants", "oneL: " + oneL);
+
+        Log.i("Pants", "twoS: " + twoS);
+        Log.i("Pants", "twoM: " + twoM);
+        Log.i("Pants", "twoL: " + twoL);
+
+        Log.i("Pants", "comS: " + comS);
+        Log.i("Pants", "comM: " + comM);
+        Log.i("Pants", "comL: " + comL);
     }
 
 
