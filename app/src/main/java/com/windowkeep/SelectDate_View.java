834bd8e100bb785af37919ebfa9d14a84b76a040
tannerbeeson <bee15005@byui.edu>
2019-12-09 12:43:03 -0700

    Cannot select a date for an appointment if the date already passed

--- app/src/main/java/com/windowkeep/SelectDate_View.java
+++ app/src/main/java/com/windowkeep/SelectDate_View.java
@@ -4,13 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
 
 public class SelectDate_View extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -22,6 +29,7 @@ public class SelectDate_View extends AppCompatActivity implements AdapterView.On
     private String dateTime;
     private String hours;
     private String minutes;
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +37,57 @@ public class SelectDate_View extends AppCompatActivity implements AdapterView.On
         setContentView(R.layout.activity_select_date__view);
         calendarView = findViewById(R.id.calendarView);
         t_selectedDate = findViewById(R.id.t_selectedDate);
+        confirmButton = findViewById(R.id.b_confirmDate);
+
+        confirmButton.setEnabled(false);
+        confirmButton.setVisibility(View.INVISIBLE);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
-                dateMonth = month + 1;
-                dateDay = day;
-                dateYear = year;
-                t_selectedDate.setText(dateMonth + "/" + dateDay + "/" + dateYear);
+                java.util.Date date = Calendar.getInstance().getTime();
+                String monthString = (String) DateFormat.format("MM", date);
+                String dayString = (String) DateFormat.format("dd", date);
+                String yearString = (String) DateFormat.format("yyyy", date);
+                int dayNum = 0;
+                int monthNum = 0;
+                int yearNum = 0;
+
+                // Convert all the string values to integers with these try statements
+                try {
+                    dayNum = Integer.parseInt(dayString);
+                } catch(NumberFormatException nfe) {
+                }
+
+                try {
+                    yearNum = Integer.parseInt(yearString);
+                } catch(NumberFormatException nfe) {
+                }
+
+                try {
+                    monthNum = Integer.parseInt(monthString);
+                } catch(NumberFormatException nfe) {
+                }
+
+
+                // THIS LOGIC IS FOR WHEN IT DOES NOT WORK
+                // current date = 12/9/2019      selected date = 11/14/2019
+                if      (yearNum > year || (monthNum > (month + 1) && yearNum == year) ||
+                        (monthNum == month + 1 && yearNum == year && dayNum > day)) {
+                    t_selectedDate.setText("Invalid Date");
+                    confirmButton.setEnabled(false);
+                    confirmButton.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    dateMonth = month + 1;
+                    dateDay = day;
+                    dateYear = year;
+                    t_selectedDate.setText(dateMonth + "/" + dateDay + "/" + dateYear);
+                    confirmButton.setEnabled(true);
+                    confirmButton.setVisibility(View.VISIBLE);
+                }
+
+
             }
         });
 
